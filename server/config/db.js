b6686f0541e39808e50d3e2fbeed7ea359045481
Devunlock <dokeyo390@gmail.com>
2026-07-15 11:18:52 +0300

    Resolve exposed server secrets

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-   await mongoose.connect("mongodb+srv://Rhymes:zMR5qSJlWtx9t9CK@cluster0.i5nj2tj.mongodb.net/?appName=Cluster0").then(() =>{
+   await mongoose.connect(process.env.MONGODB_URI).then(() =>{
     console.log("DB connected successfully");
    })
 };
